bcffc63ec51b53dbf911b25ef1b05261d1fc6986
Rashib Khanal <contact@rashib.dev>
2024-12-26 15:57:21 -0500

    update: change the website color and put a animated hamburger icon on click

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,37 @@ function showSideIcons() {
     // if ()
     if (sideBarElement.style.display === 'flex') {
         sideBarElement.style.display = 'none';
+
+        const spanFirstLineElement = document.querySelector('.line1');
+        spanFirstLineElement.style.transform = 'rotate(0deg)';
+
+        const spanThirdLineElement = document.querySelector('.line3');
+        spanThirdLineElement.style.transform = 'rotate(0deg)';
+
+
+        const spanSecondLineElement = document.querySelector('.line2');
+  
+        spanSecondLineElement.style.display = 'flex';
+        spanSecondLineElement.style.transition = '0.8s ease-out';
+
     }
     else {
         sideBarElement.style.display = 'flex';
+        const spanFirstLineElement = document.querySelector('.line1');
+        spanFirstLineElement.style.transform = 'rotate(50deg)';
+        // spanFirstLineElement.style.transition-duration = '1s';
+
+        const spanThirdLineElement = document.querySelector('.line3');
+        spanThirdLineElement.style.transform = 'rotate(-50deg)';
+
+        const spanSecondLineElement = document.querySelector('.line2');
+
+        spanSecondLineElement.style.display = 'none';
+        spanSecondLineElement.style.transition = '0.8s ease-out';
     }
+
+
+    
 
 }
 
