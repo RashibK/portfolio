d99ea955b5092e2b0e56d941b685700d12bb2684
Rashib Khanal <contact@rashib.dev>
2025-07-12 11:37:57 -0500

    add: nav bar

---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router";
+
+
+const Header: React.FC = () => {
+  return (
+    <header className="w-full shadow-header fixed z-1 animate-slideUp bottom-[0] py-[10px] px-0">
+      <div className="max-w-[1600px] flex justify-start items-center py-[0.5em] text-[1.2rem] mx-auto my-0 leading-[30.7167px] tracking-[0.96px]">
+        <div>
+          <div className="font-unica-one text-[1.2em] inline-block mr-[0.8em] leading-[36.8667px]">
+            <Link to="/">/Rashib Khanal/</Link>
+          </div>
+          <nav className="inline-block text-[1.1em] opacity-[0.8] leading-[33.8px]">
+            <Link
+              className="font-open-sans inline-block text-[21.12px] text-center ml-[0.8em] hover:text-[#fe5185] transition-colors duration-[0.4s] ease-out leading-[38.4px] tracking-[0.96px]"
+              to="/about"
+            >
+              About
+            </Link>
+            <a
+              className="font-open-sans text-[21.12px] text-center ml-[0.8em] hover:text-[#fe5185] transition-colors duration-[0.4s] ease-out leading-[38.4px] tracking-[0.96px]"
+              target="_blank"
+              href="mailto:contact@rashib.dev"
+            >
+              Contact
+            </a>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,45 @@
+import type React from "react";
+import { Link } from "react-router";
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <div className="min-h-screen m-auto max-w-[93%] flex flex-col justify-center items-center text-[1.5rem] animate-main-fadeIn">
+        <div className="flex grow flex-col justify-center">
+          <h1 className="font-unica-one tracking-[0.96px] text-[1.6em] font-bold leading-[61.43px] text-center">
+            /Rashib Khanal/
+          </h1>
+          <p className="font-noto-sans mb-[1.5em] text-center text-[0.7em] leading-normal tracking-[0.96px] opacity-[0.9]">
+            Developer building stuffs on internet :)
+          </p>
+          <nav className="">
+            <Link
+              to="/about"
+              className="font-open-sans block text-center mt-[0.5em] hover:text-[#fe5185] transition-colors duration-[0.4s] ease-out leading-[38.4px] tracking-[0.96px]"
+            >About</Link>
+            <a
+              className="font-open-sans block text-center mt-[0.5em] hover:text-[#fe5185] transition-colors duration-[0.4s] ease-out leading-[38.4px] tracking-[0.96px]"
+              target="_blank"
+              href="mailto:contact@rashib.dev"
+            >
+              Contact
+            </a>
+          </nav>
+        </div>
+        <div className="opacity-[0.6] ">
+          <p className="text-[0.7em] mb-[0.9em]">
+            &copy; 2025{" "}
+            <a
+              className="hover:text-[#fe5185] transition-colors duration-400 ease-out"
+              href="/"
+            >
+              Rashib Khanal
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
