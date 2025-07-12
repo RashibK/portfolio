16a5a1682a302dd125fb2c1bb095fb8392013668
Rashib Khanal <contact@rashib.dev>
2025-07-11 19:12:12 -0500

    add: clean minimal homepage ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="">Hey</div>
+      <div className="h-screen m-auto max-w-[93%] flex flex-col justify-center items-center">
+        <div className="flex grow flex-col justify-center">
+          <h1 className="font-unica-one tracking-[0.96px] text-[61.433px] font-bold">
+            /Rashib Khanal/
+          </h1>
+        </div>
+        <div className="footer">&copy; 2025 Rashib Khanal</div>
+      </div>
     </>
   );
 }
