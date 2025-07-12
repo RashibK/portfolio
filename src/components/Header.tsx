032fcaa5b15b9b060f2e78b12f0da344a6877dda
Rashib Khanal <contact@rashib.dev>
2025-07-12 18:41:01 -0500

    update: animations for header on about page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router";
 
-
 const Header: React.FC = () => {
+  const lastScroll = useRef(0);
+  const [scrollDirection, setScrollDirection] = useState<"up" | "down">("up");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+
+      if (scrollY > lastScroll.current) {
+        setScrollDirection("down");
+      } else if (scrollY < lastScroll.current) {
+        setScrollDirection("up");
+      }
+      lastScroll.current = scrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const animationClass = scrollDirection === "up" ? "animate-slideUp" : "animate-slideDown";
   return (
-    <header className="w-full bg-white shadow-header fixed z-1 animate-slideUp bottom-[0] py-[11.5px] px-0">
+    <header
+      className={`w-full bg-white shadow-header fixed z-1 ${animationClass} bottom-0 py-[11.5px] px-0 overflow-auto`}
+    >
       <div className="max-w-[1600px] flex justify-start items-center py-[0.5em] text-[1.2rem] mx-auto my-0 leading-[30.7167px] tracking-[0.96px]">
         <div>
           <div className="font-unica-one text-[1.2em] inline-block mr-[0.8em] leading-[36.8667px]">
