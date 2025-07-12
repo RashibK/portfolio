9215e95026a658581dbcf19211908d80f695ffa5
Rashib Khanal <contact@rashib.dev>
2025-07-11 23:42:58 -0500

    add: simple animation to the homepage at load

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="min-h-screen m-auto max-w-[93%] flex flex-col justify-center items-center text-[1.5rem]">
+      <div className="min-h-screen m-auto max-w-[93%] flex flex-col justify-center items-center text-[1.5rem] animate-main-fadeIn">
         <div className="flex grow flex-col justify-center">
           <h1 className="font-unica-one tracking-[0.96px] text-[1.6em] font-bold leading-[61.43px] text-center">
             /Rashib Khanal/
@@ -12,13 +12,17 @@ function App() {
             Developer building stuffs on internet :)
           </p>
           <nav className="">
-            <a className="block text-center mt-[0.5em]" href="/">
+            <a
+              className="font-open-sans block text-center mt-[0.5em] hover:text-[#fe5185] transition-colors duration-[0.4s] ease-out leading-[38.4px] tracking-[0.96px]"
+              href="/"
+            >
               About
             </a>
             <a
-              className="block text-center mt-[0.5em]"
-              href="mailto@rashib.dev"
+              className="font-open-sans block text-center mt-[0.5em] hover:text-[#fe5185] transition-colors duration-400 ease-out leading-[38.4px] tracking-[0.96px]"
               target="_blank"
+              href="mailto:contact@rashib.dev"
+              
             >
               Contact
             </a>
@@ -26,7 +30,13 @@ function App() {
         </div>
         <div className="opacity-[0.6] ">
           <p className="text-[0.7em] mb-[0.9em]">
-            &copy; 2025 <a href="/">Rashib Khanal</a>
+            &copy; 2025{" "}
+            <a
+              className="hover:text-[#fe5185] transition-colors duration-400 ease-out"
+              href="/"
+            >
+              Rashib Khanal
+            </a>
           </p>
         </div>
       </div>
